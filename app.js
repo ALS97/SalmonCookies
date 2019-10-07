ac0a86c244c4d2af32c39b484ce65935915aa6a1
Austin Smith <austinleesmith97@gmail.com>
2019-10-07 16:16:57 -0700

    working first list!!!

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function hour(min,max){
 }
 
 var Shop1 = {
-  shopHours: ['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'],
+  shopHours:['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'],
   minCust: 10,
   maxCust: 60,
   avgCookie: 13,
@@ -16,14 +16,15 @@ var Shop1 = {
   locationName: 'Seattle',
 
   render: function(){
-    for(var i=0;i<this.shopHours.length; i++)
+    for(var i=0;i<this.shopHours.length; i++){
       var childEl = document.createElement('li');
-    this.shopHours[i];
-    this.hourTotals[i] = hour();
-    childEl.textContent = 'quack';
-    childEl.textContent = `Shop Hours: ${this.shopHours[i]} ~ Total Customers: ${this.hourTotals[i]}`;
-    parentEL.appendChild(childEl);
+      this.shopHours[i];
+      this.hourTotals[i] = hour(10, 60);
+      // childEl.textContent = 'quack';
+      childEl.textContent = `Shop Hours: ${this.shopHours[i]} ~ Total Customers: ${this.hourTotals[i]}`;
+      parentEL.appendChild(childEl);
     // this.hourTotals[i]= hour();
+    }
   }
 };
 
