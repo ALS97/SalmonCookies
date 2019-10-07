cc316b633a7e88337a4374611a4a7ea164a4361c
Austin Smith <austinleesmith97@gmail.com>
2019-10-07 15:45:12 -0700

    first location almost done

---
+++ app.js
@@ -0,0 +1,30 @@
+
+var parentEL = document.getElementById('parentElement');
+
+function hour(min,max){
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max-min)) + min;
+}
+
+var Shop1 = {
+  shopHours: ['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'],
+  minCust: 10,
+  maxCust: 60,
+  avgCookie: 13,
+  hourTotals:[],
+  locationName: 'Seattle',
+
+  render: function(){
+    for(var i=0;i<this.shopHours.length; i++)
+      var childEl = document.createElement('li');
+    this.shopHours[i];
+    this.hourTotals[i] = hour();
+    childEl.textContent = 'quack';
+    childEl.textContent = `Shop Hours: ${this.shopHours[i]} ~ Total Customers: ${this.hourTotals[i]}`;
+    parentEL.appendChild(childEl);
+    // this.hourTotals[i]= hour();
+  }
+};
+
+Shop1.render();
